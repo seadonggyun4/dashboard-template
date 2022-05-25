999b4bad978b4e66988620db7a8a21c2016502db
seadonggyun4 <seadonggyun@gmail.com>
2022-05-25 10:21:12 +0900

    ✨ table button 누를시 정렬

--- js/main.js
+++ js/main.js
@@ -47,3 +47,54 @@ usedBxes.forEach((usedBx) => {
     .addTo(new ScrollMagic.Controller());
 })
 
+// ========================= [ filter table] =========================
+
+window.addEventListener('load', () => {
+    // isotope 라이브러리 사용
+    const grid =  new Isotope("tbody", {
+      itemSelector: 'tbody tr', // 적용할 아이템
+      columnWidth: 100, // 아티클 요소 넓이 값
+      transitionDuration: "0.5s" // 전환되는 속도
+  }) //grid로 반환
+
+  const btns = document.querySelectorAll('.recentOrders .cardHeader .btn-wrap .btn')
+
+  btns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      // 함수 중복실행 방지
+      const isOn = e.currentTarget.classList.contains('on')
+      if(isOn === false){
+          activation(e)
+      }
+    })
+
+
+
+
+      function activation(event){
+
+        //버튼 활성화
+        btns.forEach( (btn) => {
+            btn.classList.remove('on')
+        })
+        event.currentTarget.classList.add('on')
+
+        console.log(event.currentTarget)
+
+        // 탐색을 위한 값 a_href 탐색
+        const btn_a = event.currentTarget
+        const a_href = btn_a.getAttribute('href')
+
+        
+
+
+        //grid값에 arrange매서드 사용 -> filter에 들어와있는 값을 통해 해당 클래스를 탐색
+        grid.arrange({filter: a_href}) 
+    }
+
+  })
+
+
+})
